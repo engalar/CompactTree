961e5962f4bfa51d50e1a799a0df02c20f273f7e
engalar <engalar@gmail.com>
2021-12-28 18:01:19 +0800

    wip

--- src/CompactTree.webmodeler.tsx
+++ src/CompactTree.webmodeler.tsx
@@ -1,7 +1,11 @@
 import { Component, ReactNode, createElement } from "react";
-import { CompactTreePreviewProps } from "../typings/CompactTreeProps";
+import { CompactTreeContainerProps, CompactTreePreviewProps } from "../typings/CompactTreeProps";
 
 declare function require(name: string): string;
+
+type VisibilityMap<T> = {
+    [P in keyof T]: any;
+};
 
 export class preview extends Component<CompactTreePreviewProps> {
     render(): ReactNode {
@@ -11,4 +15,17 @@ export class preview extends Component<CompactTreePreviewProps> {
 
 export function getPreviewCss(): string {
     return require("./ui/CompactTree.scss");
+}
+
+
+export function getVisibleProperties(
+    valueMap: CompactTreeContainerProps,
+    visibilityMap: VisibilityMap<CompactTreeContainerProps>
+): VisibilityMap<CompactTreeContainerProps> {
+    visibilityMap.eventNodeOnClickMicroflow = valueMap.eventNodeOnClickAction === 'microflow';
+    visibilityMap.eventNodeOnClickNanoflow = valueMap.eventNodeOnClickAction === 'nanoflow';
+    visibilityMap.eventNodeOnClickForm = valueMap.eventNodeOnClickAction === 'open';
+    visibilityMap.eventNodeOnClickOpenPageAs = valueMap.eventNodeOnClickAction === 'open';
+
+    return visibilityMap;
 }
